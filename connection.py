272da3d0ab71d60fb679b3b394b2101b79a49cbf
dennoxkabugi <162958672+dennoxkabugi@users.noreply.github.com>
2024-06-13 11:43:07 +0300

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,9 @@
+# File to establish connection to the database
+import sqlite3
+
+DATABASE_NAME = './database/magazine.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
